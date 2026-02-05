df81d73ee1dcc7d883be14a459e2b080725360d1
anri103 <ianrianri@gmail.com>
2026-02-05 23:11:55 +0300

    -

--- gulpfile.js
+++ gulpfile.js
@@ -1,143 +1,143 @@
-const { src, dest, watch, series, parallel } = require('gulp');
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const rename = require('gulp-rename');
 const browserSync = require('browser-sync').create();
 const fileInclude = require('gulp-file-include');
-const sass = require('gulp-sass')(require('sass'));
-const sourcemaps = require('gulp-sourcemaps');
 const del = require('del');
-const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
 
-// Пути
-const paths = {
-  html: {
-    src: 'src/pages/**/*.html',
-    dest: 'dist/'
-  },
-  styles: {
-    src: 'src/scss/**/*.scss',
-    dest: 'dist/css/'
-  },
-  scripts: {
-    src: 'src/js/**/*.js',
-    dest: 'dist/js/'
-  },
-  vendorCSS: {
-    src: [
-      'node_modules/bootstrap/dist/css/bootstrap.min.css',
-      'node_modules/swiper/swiper-bundle.min.css',
-      'node_modules/@fancyapps/ui/dist/fancybox/fancybox.css'
-    ],
-    dest: 'dist/css/'
-  },
-  vendorJS: {
-    src: [
-      'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
-      'node_modules/swiper/swiper-bundle.min.js',
-      'node_modules/@fancyapps/ui/dist/fancybox/fancybox.umd.js'
-    ],
-    dest: 'dist/js/'
-  },
-  img: {
-    src: 'src/img/**/*',
-    dest: 'dist/img/'
-  },
-  fonts: {
-    src: 'src/fonts/**/*',
-    dest: 'dist/fonts/'
-  }
-};
-
-// Очистка dist
+// ===== Очистка dist =====
 function clean() {
-  return del(['dist']);
+  return del(['dist/**/*']);
 }
 
-// HTML (include компонентов)
+// ===== HTML include =====
 function html() {
-  return src(paths.html.src)
+  return gulp.src(['src/html/**/*.html', '!src/html/partials/**/*'])
     .pipe(fileInclude({
       prefix: '@@',
       basepath: '@file'
     }))
-    .pipe(dest(paths.html.dest))
+    .pipe(gulp.dest('dist/'))
     .pipe(browserSync.stream());
 }
 
-// SCSS
-function styles() {
-  return src(paths.styles.src)
+// ===== SCSS: стили проекта (для разработки с sourcemaps) =====
+function scssStyleDev() {
+  return gulp.src('src/scss/main.scss')
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(rename('style.css'))
     .pipe(sourcemaps.write('.'))
-    .pipe(dest(paths.styles.dest))
+    .pipe(gulp.dest('dist/css/'))
     .pipe(browserSync.stream());
 }
 
-// JS
+// ===== SCSS: стили проекта (для продакшена без sourcemaps) =====
+function scssStyleBuild() {
+  return gulp.src('src/scss/main.scss')
+    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+    .pipe(rename('style.css'))
+    .pipe(gulp.dest('dist/css/'))
+    .pipe(browserSync.stream());
+}
+
+// ===== Вендоры: CSS =====
+function vendorsCss() {
+  return gulp.src([
+    'node_modules/bootstrap/dist/css/bootstrap.min.css',
+    'node_modules/swiper/swiper-bundle.min.css',
+    'node_modules/@fancyapps/ui/dist/fancybox/fancybox.css'
+  ])
+  .pipe(gulp.dest('src/css/'));
+}
+
+// ===== Вендоры: JS =====
+function vendorsJs() {
+  return gulp.src([
+    'node_modules/swiper/swiper-bundle.min.js',
+    'node_modules/@fancyapps/ui/dist/fancybox/fancybox.umd.js',
+    'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+  ])
+  .pipe(gulp.dest('src/js/'));
+}
+
+// ===== Копирование css/ =====
+function css() {
+  return gulp.src('src/css/**/*.css')
+    .pipe(gulp.dest('dist/css/'))
+    .pipe(browserSync.stream());
+}
+
+// ===== Копирование js/ =====
 function scripts() {
-  return src(paths.scripts.src)
-    .pipe(dest(paths.scripts.dest))
+  return gulp.src('src/js/**/*.js')
+    .pipe(gulp.dest('dist/js/'))
     .pipe(browserSync.stream());
 }
 
-// Копирование vendor CSS
-function vendorCSS() {
-  return src(paths.vendorCSS.src)
-    .pipe(dest(paths.vendorCSS.dest))
-    .pipe(browserSync.stream());
-}
-
-// Копирование vendor JS
-function vendorJS() {
-  return src(paths.vendorJS.src)
-    .pipe(dest(paths.vendorJS.dest))
-    .pipe(browserSync.stream());
-}
-
-// Копирование изображений
+// ===== Копирование img/ =====
 function images() {
-  return src(paths.img.src, { encoding: false, allowEmpty: true })
-    .pipe(dest(paths.img.dest))
+  return gulp.src('src/img/**/*', { encoding: false })
+    .pipe(gulp.dest('dist/img/'))
     .pipe(browserSync.stream());
 }
 
-// Копирование шрифтов
+// ===== Копирование fonts/ =====
 function fonts() {
-  return src(paths.fonts.src, { encoding: false, allowEmpty: true })
-    .pipe(dest(paths.fonts.dest))
+  return gulp.src('src/fonts/**/*', { encoding: false })
+    .pipe(gulp.dest('dist/fonts/'))
     .pipe(browserSync.stream());
 }
 
-// Слежка
-function watchFiles() {
+// ===== Сервер BrowserSync =====
+function serve() {
   browserSync.init({
     server: {
-      baseDir: 'dist'
-    }
+      baseDir: "dist/"
+    },
+    port: 3000,
+    open: true,
+    notify: false
   });
-  watch(['src/pages/**/*.html', 'src/components/**/*.html'], html);
-  watch(['src/scss/**/*.scss', 'src/components/**/*.scss'], styles);
-  watch('src/js/**/*.js', scripts);
-  watch('src/img/**/*', images);
-  watch('src/fonts/**/*', fonts);
 }
 
+// ===== Наблюдение за изменениями =====
+function watch() {
+  gulp.watch('src/html/**/*.html', html);
+  gulp.watch('src/scss/**/*.scss', scssStyleDev);
+  gulp.watch('src/css/**/*.css', css);
+  gulp.watch('src/js/**/*.js', scripts);
+  gulp.watch('src/img/**/*', images);
+  gulp.watch('src/fonts/**/*', fonts);
+}
+
+// ===== Сборка =====
+const build = gulp.series(
+  clean,
+  gulp.parallel(html, scssStyleBuild, css, scripts, images, fonts)
+);
+
+// ===== Разработка =====
+const dev = gulp.series(
+  clean,
+  gulp.parallel(html, scssStyleDev, css, scripts, images, fonts),
+  gulp.parallel(serve, watch)
+);
+
+// ===== Объединённая задача для вендоров =====
+const vendors = gulp.parallel(vendorsCss, vendorsJs);
+
+// ===== Экспорт задач =====
 exports.clean = clean;
 exports.html = html;
-exports.styles = styles;
+exports.scssStyleDev = scssStyleDev;
+exports.scssStyleBuild = scssStyleBuild;
+exports.css = css;
 exports.scripts = scripts;
-exports.vendorCSS = vendorCSS;
-exports.vendorJS = vendorJS;
 exports.images = images;
 exports.fonts = fonts;
-
-exports.default = series(
-  clean,
-  parallel(html, styles, scripts, vendorCSS, vendorJS, images, fonts),
-  watchFiles
-);
-
-exports.build = series(
-  clean,
-  parallel(html, styles, scripts, vendorCSS, vendorJS, images, fonts)
-);
+exports.vendors = vendors; // Выполнить один раз
+exports.build = build;
+exports.dev = dev;
+exports.default = dev;
